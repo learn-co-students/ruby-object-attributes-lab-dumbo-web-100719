8bbf71ee36482e1152ad1ec0e7ad6f1fb5a329bc
Diego Martinez <dmartinezm@gmail.com>
2019-10-08 15:47:51 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+    def initialize(namee,job)
+        @name
+        @job
+    end
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
+
+end
+
+
+
