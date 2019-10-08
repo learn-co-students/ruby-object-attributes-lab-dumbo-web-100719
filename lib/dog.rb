4b23f941b8b1ba19a844e4daee68d5453709a49d
Nabeel Siddiqui <nabeelsiddiqui@Nabeels-Air.fios-router.home>
2019-10-08 16:32:18 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+    attr_accessor :name 
+    attr_accessor :breed
+end
+    # def name=(dog_name)
+    #     @name = dog_name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+    # def breed=(breed_type)
+    #     @breed = breed_type
+    # end
+
+    # def breed
+    #     @breed
+    # end
+
+
+        
+
+
