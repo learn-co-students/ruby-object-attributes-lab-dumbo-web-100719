f06d9916f452af7b6b54fb02f5b7769f4cffba65
Dwayne <dwaynemcfarlaneny@gmail.com>
2019-10-09 09:00:41 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,31 @@
+class Person
+ 
+    def initialize(name, job)
+      @name = Dwayne
+    end
+   
+    def name
+      @name
+    end
+   
+    def name= (new_name)
+      @name = Jim
+    end
+   
+  end
+
+  
+ 
+    def initialize(job)
+      @job = Banker
+    end
+   
+    def job
+      @job
+    end
+   
+    def job= (new_job)
+      @job = developer
+    end
+   
+  end
